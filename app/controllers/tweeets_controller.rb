f1432dc970eb2e8477964f0a5e70ddd71171b548
Sanad <s_wj_88@hotmail.com>
2020-09-22 22:45:27 +0300

    Add auth and markups plus modify the routs

--- app/controllers/tweeets_controller.rb
+++ app/controllers/tweeets_controller.rb
@@ -1,6 +1,6 @@
 class TweeetsController < ApplicationController
   before_action :set_tweeet, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, except: [:show, :index]
+  before_action :authenticate_user!, except: [:index, :show]
 
   # GET /tweeets
   # GET /tweeets.json
@@ -16,7 +16,7 @@ class TweeetsController < ApplicationController
 
   # GET /tweeets/new
   def new
-    @tweeet = Tweeet.new
+    @tweeet = current_user.tweeets.build
   end
 
   # GET /tweeets/1/edit
@@ -26,7 +26,7 @@ class TweeetsController < ApplicationController
   # POST /tweeets
   # POST /tweeets.json
   def create
-    @tweeet = Tweeet.new(tweeet_params)
+    @tweeet = current_user.tweeets.build(tweeet_params)
 
     respond_to do |format|
       if @tweeet.save
@@ -69,7 +69,7 @@ class TweeetsController < ApplicationController
       @tweeet = Tweeet.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
+    # Never trust parameters from the scary internet, only allow the white list through.
     def tweeet_params
       params.require(:tweeet).permit(:tweeet)
     end
